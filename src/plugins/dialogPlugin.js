1011e3574129c47d21fc8202635b7230206e1a09
otaviovasc-inteli <otavio.vasconcelos@sou.inteli.edu.br>
2024-03-05 22:17:17 -0300

    oversight bug fix on dialog animation delay

--- src/plugins/dialogPlugin.js
+++ src/plugins/dialogPlugin.js
@@ -106,7 +106,7 @@ export default class DialogModalPlugin {
     this._setText(tempText, this.depth);
     if (animate) {
       this.timedEvent = this.scene.time.addEvent({
-        delay: 200 - (this.dialogSpeed * 45),
+        delay: 25,
         callback: this._animateText,
         callbackScope: this,
         loop: true
