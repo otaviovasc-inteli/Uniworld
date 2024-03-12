cb371e28700a7ddc4774403bfe30be0f64248cac
otaviovasc-inteli <otavio.vasconcelos@sou.inteli.edu.br>
2024-03-11 23:07:41 -0300

    Quiz V1

--- src/texts/rexonaTexts.js
+++ src/texts/rexonaTexts.js
@@ -1,7 +1,7 @@
 // Each line represent one interaction in that dialog box, you can edit text as you want
 const rexonaTexts =
 [
-  '"Rexona" adquirido!',
-  'Para utilizar seus novos poderes utilize a tecla Q'
+  ['Com quantos paus se faz uma canoa?', ['A) 42', 'B) 3', 'C) mais de 1', 'A']], // Question with answers and correct answer
+  ['Qual o nome do cavalo branco de napoleao?', ['A) Preto Claro', 'B) Verde Musgo', 'C) Branco escuro', 'B']]
 ];
 export default rexonaTexts;
