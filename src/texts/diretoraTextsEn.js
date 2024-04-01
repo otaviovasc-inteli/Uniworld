548127adf1001eaaa58d14c1337a3caac1e98f44
Marlos Guedes <marlos.guedes@sou.inteli.edu.br>
2024-04-01 15:44:16 -0300

    Diretora Logic and texts

---
+++ src/texts/diretoraTextsEn.js
@@ -0,0 +1,11 @@
+const diretoraTextsEn =
+[
+  "Director: Incredible, you've done it, hero! Welcome back to Unilever!",
+  "Director: Thanks to your bravery and determination, all our brands have been recovered.",
+  "Director: You've not only restored Unilever's balance but also reinforced our unity and resilience as a team.",
+  "Director: Your achievement goes beyond just reclaiming the brands; you've inspired all of us to be better and stronger together.",
+  "Director: On behalf of the entire Unilever team, I would like to express our deepest gratitude. You are a true hero to us.",
+  "Director: Let's continue to communicate and collaborate through Microsoft Teams. Who knows what new adventures await us?",
+  "Director: For now, celebrate, hero! You more than deserve it. We look forward to seeing you shine even more in the future."
+];
+export default diretoraTextsEn;
---
+++ src/texts/diretoraTextsPt.js
@@ -0,0 +1,11 @@
+const diretoraTextsPt =
+[
+  'Diretora: Incrível, você conseguiu, herói! Bem-vindo à Unilever!',
+  'Diretora: Graças a sua coragem e determinação, todas as nossas marcas foram recuperadas.',
+  'Diretora: Você não apenas restaurou o equilíbrio da Unilever, mas também reforçou nossa união e resiliência como equipe.',
+  'Diretora: Seu feito vai além de apenas recuperar as marcas; você inspirou a todos nós a sermos melhores e mais fortes juntos.',
+  'Diretora: Em nome de toda a equipe Unilever, eu gostaria de expressar nossa mais profunda gratidão. Você é um verdadeiro herói para nós.',
+  'Diretora: Vamos continuar a comunicar e colaborar pelo Microsoft Teams. Quem sabe que novas aventuras nos aguardam?',
+  'Diretora: Por agora, celebre, herói! Você mais do que merece. Estamos ansiosos para vê-lo brilhar ainda mais no futuro.'
+];
+export default diretoraTextsPt;
